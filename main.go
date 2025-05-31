e770048ca737d097282286fd33e531e46c8434ef
polonkoevv <polonkoevv.v2@gmail.com>
2025-05-31 12:23:13 +0300

    file

--- main.go
+++ main.go
@@ -67,7 +67,7 @@ func main() {
 	log.SetLevel(logLevel)
 
 	// Открываем файл для логов
-	file, err := os.OpenFile("/app/logs/app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	file, err := os.OpenFile("./logs/app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Warn("Не удалось открыть файл логов, логирование только в консоль")
 	} else {
