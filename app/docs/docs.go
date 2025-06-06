c6c1699fb9b3481147b735a97f993454e5765e21
polonkoevv <polonkoevv.v2@gmail.com>
2025-06-07 01:50:24 +0300

    fixed

--- app/docs/docs.go
+++ app/docs/docs.go
@@ -246,7 +246,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "127.0.0.1",
 	BasePath:         "/api",
 	Schemes:          []string{},
 	Title:            "Library API",
--- test_balancing.sh
+++ test_balancing.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-
-# Количество запросов
-requests=50
+requests=10
 
 echo "Тестирование балансировки нагрузки..."
 echo "Отправка $requests запросов..."
 
 for i in $(seq 1 $requests)
 do
-    curl -s -I -H "Host: example.com" http://http://83.222.17.152:80/ | grep X-Upstream
+    curl --insecure -s -I -H "Host: localhost" https://127.0.0.1/api/books | grep X-Upstream
     sleep 0.1
 done
 
